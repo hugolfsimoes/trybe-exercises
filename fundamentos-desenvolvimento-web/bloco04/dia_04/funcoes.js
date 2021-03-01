5249f6dff744c40ccfa98b64821f1e7371e06977
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-01 20:37:05 -0300

    atualizando os exercicios

--- fundamentos-desenvolvimento-web/bloco04/dia_04/funcoes.js
+++ fundamentos-desenvolvimento-web/bloco04/dia_04/funcoes.js
@@ -1,70 +1,84 @@
 //Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
-let palavra = "amor";
-
-function palindromo(palavra) {
-  let contrario = ("");
-  console.log(contrario);
-  contrario = "";
-  console.log(contrario);
-  for (let index = palavra.length; index >= 0; index--) {
-    contrario += palavra[index];
+function verificaPalindromo(palavra) {
+  let arrayLetras = palavra.split("");
+  let isPalindrome = true;
+  for (let index in arrayLetras) {
+    if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
+      isPalindrome = false;
+    }
   }
-  console.log(contrario);
+  return isPalindrome;
 }
 
-
-palindromo(palavra);
+console.log(verificaPalindromo("arara"));
 
 // 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 let array = [2, 3, 6, 7, 10, 1];
 
-function maiorNumero(array) {
+function indiceMaiorNumero(numeros) {
   var maior = 0;
   var posicao;
-  for (let index = 0; index < array.length; index += 1) {
-    if (array[index] > maior) {
-      maior = array[index];
+  for (let index in numeros) {
+    if (numeros[index] > maior) {
+      maior = numeros[index];
       posicao = index;
     }
   }
   return posicao;
 }
 
-console.log(maiorNumero(array));
+console.log(indiceMaiorNumero(array));
 
 // 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 let array2 = [2, 4, 6, 7, 10, 0, -3];
 
-function menorNumero(array) {
+function indiceMenorNumero(numeros) {
   let menor = 0;
   let posicao;
-  for (let index = 0; index < array2.length; index += 1) {
-    if (array[index] < menor) {
-      menor = array[index];
+  for (let index in numeros) {
+    if (numeros[index] < menor) {
+      menor = numeros[index];
       posicao = index;
     }
   }
   return posicao;
 }
 
-console.log(menorNumero(array2));
+console.log(indiceMenorNumero(array2));
+
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+
+function maiorPalavra(palavras) {
+  let maiorPalavra = palavras[0];
+  for (let indice in palavras) {
+    if (maiorPalavra.length < palavras[indice].length) {
+      maiorPalavra = palavras[indice];
+    }
+  }
+  return maiorPalavra;
+}
+
+console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
 
 // 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 let array3 = [2, 3, 2, 5, 8, 2, 3];
 
-function numeroMaisRepete(array) {
+function numeroMaisRepete(numeros) {
   cont = 0;
   let repete = 0;
   let resultado;
-  for (let index = 0; index < array.length; index += 1) {
+  for (let index in numeros) {
     cont = 0;
-    for (indexDois = 1; indexDois < array3.length; indexDois += 1) {
-      if (array3[index] === array3[indexDois]) {
+    for (indexDois in numeros) {
+      if (numeros[index] === numeros[indexDois]) {
         cont += 1;
       }
       if (cont > repete) {
         repete = cont;
-        resultado = array[index];
+        resultado = numeros[index];
       }
 
     }
@@ -76,6 +90,7 @@ function numeroMaisRepete(array) {
 console.log(numeroMaisRepete(array3));
 
 // 6 - Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+
 let n = 5;
 
 function somatorio(n) {
@@ -88,7 +103,7 @@ function somatorio(n) {
 
 console.log(somatorio(n));
 
-//Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+// 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
 let word = "trybe";
 let final = "be";
 
