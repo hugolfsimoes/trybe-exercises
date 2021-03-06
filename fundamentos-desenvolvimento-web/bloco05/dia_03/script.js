665c0948231e190b7741d26abf9090c950c21442
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-06 11:36:34 -0300

    feito o exercicio 5

--- fundamentos-desenvolvimento-web/bloco05/dia_03/script.js
+++ fundamentos-desenvolvimento-web/bloco05/dia_03/script.js
@@ -64,7 +64,6 @@ CriaBotaoFeriados("Feriados");
 
 let botaoFeriados = document.querySelector('#btn-holiday');
 let dayHoliday = document.querySelectorAll('.holiday ');
-console.log(dayHoliday);
 
 botaoFeriados.addEventListener('click', mudaCorHoliday);
 
@@ -94,3 +93,28 @@ function criaBotaoSexta(sexta) {
 }
 
 criaBotaoSexta('Sexta-feira');
+
+/* Exercício 5:
+Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias. */
+
+
+let btnFriday = document.querySelector('#btn-friday');
+let dayFriday = document.querySelectorAll('.friday');
+let lastFriday = [4, 11, 18, 25];
+
+
+btnFriday.addEventListener('click', trocaTextoSexta);
+
+function trocaTextoSexta(event) {
+
+  for (let key = 0; key < dayFriday.length; key += 1) {
+    if (dayFriday[key].innerHTML !== "SEXTOU") {
+      dayFriday[key].innerHTML = "SEXTOU";
+    } else {
+      dayFriday[key].innerHTML = lastFriday[key]
+    }
+  }
+  event.preventDefault();
+}
+
