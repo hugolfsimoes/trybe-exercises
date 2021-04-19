dcb55b57a753889545b9e742feed6f3efbfd7daf
Hugo Simoes <hugolfsimoes@gmail.com>
2021-04-19 19:33:22 -0300

    refatorando tarefa 1

--- fundamentos-desenvolvimento-web/bloco10/dia_01/parte1/exercise01.test.js
+++ fundamentos-desenvolvimento-web/bloco10/dia_01/parte1/exercise01.test.js
@@ -1,8 +1,25 @@
-const { test, expect } = require('@jest/globals')
+const { test, expect } = require('@jest/globals');
 const sum = require('./exercise01')
 
-test('Soma de dois valores', ()=>{
-  expect(sum(4,5)).toBe(9);
-  expect(sum(0,0)).toBe(0);
-  //expect(sum(4,'5')).toBe('parameters must be numbers');
-})
+describe('Soma de dois valores', () => {
+  test('4 + 5 é igual a 9', () => {
+    expect(sum(4,5)).toEqual(9);
+  });
+
+  test('0 + 0 é igual a 0', () => {
+    expect(sum(0,0)).toEqual(0);
+  });
+
+  test('throw an error when a string is passed', () => {
+    expect(()=>{
+      sum(4,'5');
+    }).toThrow();
+  });
+
+  test('error message is "parameters must be numbers"', () => {
+    expect(()=>{
+      sum(4,'5');
+    }).toThrow(/parameters must be numbers/);
+  });
+
+});
