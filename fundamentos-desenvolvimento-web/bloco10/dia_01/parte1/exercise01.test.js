546f9385fdab4a0728ad0dd5fd1adb83657cd90e
Hugo Simoes <hugolfsimoes@gmail.com>
2021-04-19 18:55:00 -0300

    feito a tarefa 2

--- fundamentos-desenvolvimento-web/bloco10/dia_01/parte1/exercise01.test.js
+++ fundamentos-desenvolvimento-web/bloco10/dia_01/parte1/exercise01.test.js
@@ -4,5 +4,5 @@ const sum = require('./exercise01')
 test('Soma de dois valores', ()=>{
   expect(sum(4,5)).toBe(9);
   expect(sum(0,0)).toBe(0);
-  expect(sum(4,'5')).toBe(9);
+  //expect(sum(4,'5')).toBe('parameters must be numbers');
 })
--- fundamentos-desenvolvimento-web/bloco10/dia_01/parte1/exercise02.test.js
+++ fundamentos-desenvolvimento-web/bloco10/dia_01/parte1/exercise02.test.js
@@ -1,4 +1,8 @@
-const { test } = require('@jest/globals');
+const { test, expect } = require('@jest/globals');
 const myRemove = require('./exercise02');
 
-test()
+test('Retorn array sem o elemento indicado', ()=>{
+  expect(myRemove([1, 2, 3, 4], 3)).toEqual([1,2,4]);
+  expect(myRemove([1,2,3,4],3)).not.toEqual([1,2,3,4]);
+  expect(myRemove([1,2,3,4],5)).toEqual([1,2,3,4]);
+})
