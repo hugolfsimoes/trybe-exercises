d3a35e940c5062e6ff85085eefcd49361123a019
Hugo Simoes <hugolfsimoes@gmail.com>
2021-05-05 18:18:49 -0300

    subindo arquivos

--- front-end/bloco12/dia_02/my-form-2.0/src/components/Form.js
+++ front-end/bloco12/dia_02/my-form-2.0/src/components/Form.js
@@ -8,6 +8,7 @@ class Forms extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleBlur = this.handleBlur.bind(this);
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleClick = this.handleClick.bind(this);
 
     this.state = {
       nome: '',
@@ -17,7 +18,9 @@ class Forms extends React.Component {
       cidade: '',
       estado: '',
       tipo: '',
+      resumo: '',
       cargo: '',
+      descricao: '',
     }
   }
 
@@ -49,67 +52,80 @@ class Forms extends React.Component {
     inputCargo.removeEventListener('mouseenter', this.handleMouseEnter);
   }
 
-  handleClick () {
-    
+  handleClick ({target}) {
+    console.log(target.name);
+    const resultado = Object.values(this.state);
+    const section = document.querySelector('.section-forms');
+    const divResult = document.createElement('div');
+   
+    resultado.forEach((result => {
+      const tagP = document.createElement('p');
+      tagP.innerText = result;
+      divResult.appendChild(tagP);
+    }));
+    section.appendChild(divResult);
+
   }
 
   render() {
 
     return(
-      <forms>
-      <fieldset className="fieldset-informacoes">
-        <label>Nome:
-        <input type="text" maxLength="40" name="nome" required onChange={this.handleChange}></input>
-        </label>
+      <section className="section-forms">
+       <forms>
+        <fieldset className="fieldset-informacoes">
+          <label>Nome:
+          <input type="text" maxLength="40" name="nome" required onChange={this.handleChange}></input>
+          </label>
 
-        <label>Email:
-        <input type="text" maxLength="50" name="email" required onChange={this.handleChange}></input>
-        </label>
+         <label>Email:
+          <input type="text" maxLength="50" name="email" required onChange={this.handleChange}></input>
+         </label>
 
-        <label>CPF:
-        <input type="text" maxLength="11" name="cpf" required onChange={this.handleChange}></input>
-        </label>
+          <label>CPF:
+         <input type="text" maxLength="11" name="cpf" required onChange={this.handleChange}></input>
+          </label>
 
-        <label>Endereço:
-        <input type="text" maxLength="200" name="endereco" required onChange={this.handleChange}></input>
-        </label>
+          <label>Endereço:
+         <input type="text" maxLength="200" name="endereco" required onChange={this.handleChange}></input>
+          </label>
 
-        <label>Cidade:
-        <input type="text" maxLength="28" name="cidade" required onChange={this.handleChange} onBlur={this.handleBlur}></input>
-        </label>
+          <label>Cidade:
+         <input type="text" maxLength="28" name="cidade" required onChange={this.handleChange} onBlur={this.handleBlur}></input>
+         </label>
 
-        <label>
-          <select name="estado" onChange={this.handleChange}>
-            <Option />
-          </select>
-        </label>
+         <label>
+           <select name="estado" onChange={this.handleChange}>
+              <Option />
+            </select>
+          </label>
 
-        <label>Casa
-          <input type="radio" name="tipo"  className="radio-btn" value="Casa" onChange={this.handleChange}></input>
-        </label>
-        <label>Apartamento
-          <input type="radio" name="tipo"  className="radio-btn" value="Apartamento" onChange={this.handleChange}></input>
-        </label>
-       </fieldset>
+          <label>Casa
+            <input type="radio" name="tipo"  className="radio-btn" value="Casa" onChange={this.handleChange}></input>
+          </label>
+          <label>Apartamento
+           <input type="radio" name="tipo"  className="radio-btn" value="Apartamento" onChange={this.handleChange}></input>
+          </label>
+         </fieldset>
 
-       <fieldset className="fieldset-emprego"> 
-       <label> Resumo do Currículo
-         <textarea maxLength="1000" required></textarea>
-       </label>
+        <fieldset className="fieldset-emprego"> 
+        <label> Resumo do Currículo
+          <textarea maxLength="1000" name="resumo" required onChange={this.handleChange}></textarea>
+         </label>
 
-       <label> Cargo
-         <textarea maxLength="40" required className="input-cargo" name="cargo"  onMouseEnter={this.handleMouseEnter}></textarea>
-       </label>
+        <label> Cargo
+          <textarea maxLength="40" required className="input-cargo" name="cargo" onChange={this.handleChange} onMouseEnter={this.handleMouseEnter}></textarea>
+         </label>
 
-       <label> Descrição do Cargo
-         <input type="text
-         " maxLength="500" required></input>
-       </label>
+        <label> Descrição do Cargo
+          <input type="text" maxLength="500" name="descricao" required onChange={this.handleChange}></input>
+         </label>
 
-       </fieldset>
+         </fieldset>
 
-        <button className="btn-enviar" onClick={this.handleClick}>Enviar</button>
-      </forms>
+          <button className="btn-enviar" name="btnEnviar" onClick={this.handleClick}>Enviar</button>
+          <button className="btn-limpar" name="btnLimpar" onClick={this.handleClick}>Limpar</button>
+        </forms>
+      </section>
       
     )
   }
