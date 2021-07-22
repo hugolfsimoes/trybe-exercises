f575838c0ac153e360f04369adeac95577f8471b
Hugo Simoes <hugolfsimoes@gmail.com>
2021-07-22 11:54:39 -0300

    feito requisitos bonus

--- back-end/bloco20/dia_04/exercises-day4.sql
+++ back-end/bloco20/dia_04/exercises-day4.sql
@@ -32,3 +32,19 @@ WHERE movie_id IN (2, 9);
 DELETE FROM Movies
 WHERE director = 'Andrew Staton';
 
+UPDATE BoxOffice
+SET rating = 9.0
+WHERE domestic_sales > 400000000;
+
+UPDATE BoxOffice
+SET rating = 6.0
+WHERE domestic_sales > 200000000 AND international_sales < 300000000;
+
+SELECT id, length_minutes FROM Movies
+WHERE length_minutes < 100;
+
+ DELETE FROM BoxOffice
+ WHERE movie_id IN (1, 6, 7, 8);
+ 
+DELETE FROM Movies
+WHERE length_minutes < 100;
