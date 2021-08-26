af40964bb562820ca532fb9fa3869d82f43d2974
hugolfsimoes <hugolfsimoes@gmail.com>
2021-08-26 19:24:05 -0300

    feito tarefa 1 do dia

---
+++ back-end/bloco26/dia_03/numberType.js
@@ -0,0 +1,3 @@
+function numberType() {}
+
+module.exports = numberType;
---
+++ back-end/bloco26/dia_03/numberType.test.js
@@ -0,0 +1,51 @@
+const { expect } = require("chai");
+
+const numberType = require("./numberType");
+
+describe("Executa a função numberType", () => {
+  describe("quando o número foi maior que zero", () => {
+    describe("a resposta", () => {
+      it("é uma string", () => {
+        const resposta = numberType(2);
+
+        expect(resposta).to.be.a("string");
+      });
+
+      it('"é igual a "positivo"', () => {
+        const resposta = numberType(2);
+
+        expect(resposta).to.be.equal("positivo");
+      });
+    });
+  });
+  describe("quando o número foi menor que zero", () => {
+    describe("a resposta", () => {
+      it("é uma string", () => {
+        const resposta = numberType(-2);
+
+        expect(resposta).to.be.a("string");
+      });
+
+      it("é igual a 'negativo'", () => {
+        const resposta = numberType(-2);
+
+        expect(resposta.to.be.equal("negativo"));
+      });
+    });
+  });
+  describe("quando o número for 0", () => {
+    describe("a resposta", () => {
+      it("é uma string", () => {
+        const resposta = numberType(0);
+
+        expect(resposta).to.be.a("string");
+      });
+
+      it("é igual a 'neutro'", () => {
+        const resposta = numberType(0);
+
+        expect(resposta).to.be.equal("neutro");
+      });
+    });
+  });
+});
