dc2d2f620aa1c1caa0e3f326817c2ca2cdee8197
hugolfsimoes <hugolfsimoes@gmail.com>
2021-09-16 18:22:54 -0300

    consertando pastas

--- back-end/bloco27/msc-crud-main/middlewares/validateUser.js
+++ back-end/bloco27/msc-crud-main/middlewares/validateUser.js
@@ -2,7 +2,6 @@ const users = require('../schemas/users');
 
 module.exports = (req, res, next) => {
   const { error } = users.validate(req.body);
-  console.log('chegou aqui')
   if (error) next(error);
   next();
 };
