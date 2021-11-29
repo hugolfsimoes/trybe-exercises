14c27b300cf70f430fdef6720376071040130de3
hugolfsimoes <hugolfsimoes@gmail.com>
2021-11-29 18:53:35 -0300

    finalizado o exercicio 01

--- ciencia-da-computacao/bloco32/aula_01/aula1.py
+++ ciencia-da-computacao/bloco32/aula_01/aula1.py
@@ -1,1 +1,10 @@
-print("Olá Mundo")
+def biggerNumber(numOne, numTwo):
+    if numOne > numTwo:
+        return numOne
+    elif numTwo > numOne:
+        return numTwo
+    else:
+        return "Same numbers"
+
+
+print(biggerNumber(3, 3))
